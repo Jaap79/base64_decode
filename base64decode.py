5836d0ec0b54cf1d3f5d0531ff92a9c8024d92ec
Jaap79 <92680483+Jaap79@users.noreply.github.com>
2023-11-08 09:09:22 +0100

    Add files via upload

--- base64decode.py
+++ base64decode.py
@@ -1,21 +1,29 @@
 import base64
 
 # Vraag de gebruiker om de base64-gecodeerde string
-encoded_string = input("Enter the base64-encoded string: ")
+encoded_string = input("Voer de base64-gecodeerde string in: ")
 
 # Proberen te decoderen van de base64-string
 try:
     decoded_bytes = base64.b64decode(encoded_string)
 except ValueError:
-    print("Invalid base64-string. Try using ASCII instead..")
+    print("Ongeldige base64-string. Probeer ASCII te gebruiken.")
     exit()
 
 # Proberen te decoderen met verschillende tekensets
 charsets = ['utf-8', 'ascii', 'windows-1252', 'ISO-8859-1', 'utf-16', 'ISO-8859-15', 'gb2312', 'gbk', 'big5', 'shift_jis', 'cp850', 'ISO-8859-2', 'ISO-8859-6', 'ISO-8859-15', 'EUC-CN', 'HZ' ]
 
+decoded_strings = []
+decode_success = False
 for charset in charsets:
     try:
         decoded_string = decoded_bytes.decode(charset)
-        print(f'Decoded string using {charset}: {decoded_string}')
+        if decoded_string not in decoded_strings:
+            decoded_strings.append(decoded_string)
+            print(f'Gedecodeerde string met {charset}: {decoded_string}')
+            decode_success = True
     except UnicodeDecodeError:
-        print(f'Unable to decode the string using {charset}')
+        continue
+
+if not decode_success:
+    print('Het decoderen is niet gelukt met geen enkele tekenset.')
